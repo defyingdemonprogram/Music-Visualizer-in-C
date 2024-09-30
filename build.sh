740750e1a57d6fdf1c22e977b10c2792383d3492
sanjeev.bhandari <sanjeev.bhandari@tai.com.np>
2024-09-30 17:47:54 +0545

    Fix build.sh: glfw is not linked automatically in macOS

--- build.sh
+++ build.sh
@@ -3,11 +3,14 @@
 set -xe
 
 CFLAGS="-Wall -Wextra -ggdb `pkg-config --cflags raylib`"
-LIBS="`pkg-config --libs raylib` -lglfw -lm -ldl -lpthread"
+LIBS="`pkg-config --libs raylib` `pkg-config --libs glfw3` -lm -ldl -lpthread"
 
 mkdir -p ./build/
-clang $CFLAGS -o ./build/libplug.so -fPIC -shared ./src/plug.c $LIBS
-# for hotreloading
-clang $CFLAGS -DHOTRELOAD -o ./build/musializer ./src/main.c $LIBS
-# clang $CFLAGS -o ./build/musializer ./src/plug.c ./src/main.c $LIBS
+if [ ! -z "${HOTRELOAD}" ]; then
+    clang $CFLAGS -o ./build/libplug.so -fPIC -shared ./src/plug.c $LIBS
+    # for hotreloading
+    clang $CFLAGS -DHOTRELOAD -o ./build/musializer ./src/main.c $LIBS
+else
+    clang $CFLAGS -o ./build/musializer ./src/plug.c ./src/main.c $LIBS
+fi
 clang -o ./build/fft ./src/fft.c -lm

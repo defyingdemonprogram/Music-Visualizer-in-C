4df9540608c281c15cd36276d0f5e52835a69080
sanjeev.bhandari <sanjeev.bhandari@tai.com.np>
2024-10-06 17:10:21 +0545

    ffmpeg_send_frame -> ffmpeg_send_frame_flipped

--- src/ffmpeg.h
+++ src/ffmpeg.h
@@ -4,7 +4,7 @@
 #include <stddef.h>  // Include stddef.h for size_t
 
 int ffmpeg_start_rendering(size_t width, size_t height, size_t fps, const char *sound_file_path);
-void ffmpeg_send_frame(int pipe, void *data, size_t width, size_t height);
+void ffmpeg_send_frame_flipped(int pipe, void *data, size_t width, size_t height);
 void ffmpeg_end_rendering(int pipe);
 
 #endif // FFMPEG_H_

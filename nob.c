97ec7d1e444143c8199e4102c2a43bf8e5dd8071
sanjeev.bhandari <sanjeev.bhandari@tai.com.np>
2024-12-12 21:47:07 +0545

    Implement nob_macos.c and remove code from nob.c

--- nob.c
+++ nob.c
@@ -9,9 +9,11 @@
 #define NOB_IMPLEMENTATION
 #include "./src/nob.h"
 
+#define CONFIG_PATH "./build/config.h"
+
 #ifdef CONFIGURED
 
-#include "./build/config.h"
+#include CONFIG_PATH
 
 #define RAYLIB_VERSION "5.0"
 
@@ -41,214 +43,30 @@ static const char *raylib_modules[] = {
 #include "nob_win64_msvc.c"
 #endif // MUSIALIZER_TARGET
 
-#if 0
-bool build_raylib(Config config) {
-    bool result = true;
-    Nob_Cmd cmd = {0};
-    Nob_File_Paths object_files = {0};
-
-    if (!nob_mkdir_if_not_exists("./build/raylib")) {
-        nob_return_defer(false);
-    }
-
-    Nob_Procs procs = {0};
-
-    const char *build_path = nob_temp_sprintf("./build/raylib/%s", NOB_ARRAY_GET(target_names, config.target));
-
-    if (!nob_mkdir_if_not_exists(build_path)) {
-        nob_return_defer(false);
-    }
-
-    for (size_t i = 0; i < NOB_ARRAY_LEN(raylib_modules); ++i) {
-        const char *input_path = nob_temp_sprintf("./raylib/raylib-"RAYLIB_VERSION"/src/%s.c", raylib_modules[i]);
-        const char *output_path = nob_temp_sprintf("%s/%s.o", build_path, raylib_modules[i]);
-
-        switch (config.target) {
-            case TARGET_LINUX:
-            case TARGET_MACOS:
-            case TARGET_WIN64_MINGW:
-                output_path = nob_temp_sprintf("%s/%s.o", build_path, raylib_modules[i]);
-                break;
-            case TARGET_WIN64_MSVC:
-                output_path = nob_temp_sprintf("%s/%s.obj", build_path, raylib_modules[i]);
-                break;
-            default: NOB_ASSERT(0 && "unreachable");
-        }
-
-        nob_da_append(&object_files, output_path);
-
-        if (nob_needs_rebuild(output_path, &input_path, 1)) {
-            cmd.count = 0;
-            switch (config.target) {
-                case TARGET_LINUX:
-                    nob_cmd_append(&cmd, "cc");
-                    nob_cmd_append(&cmd, "-ggdb", "-DPLATFORM_DESKTOP", "-fPIC");
-                    nob_cmd_append(&cmd, "-I./raylib/raylib-"RAYLIB_VERSION"/src/external/glfw/include");
-                    nob_cmd_append(&cmd, "-c", input_path);
-                    nob_cmd_append(&cmd, "-o", output_path);
-                    break;
-                case TARGET_MACOS:
-                    nob_cmd_append(&cmd, "clang");
-                    nob_cmd_append(&cmd, "-g", "-DPLATFORM_DESKTOP", "-fPIC");
-                    nob_cmd_append(&cmd, "-I./raylib/raylib-"RAYLIB_VERSION"/src/external/glfw/include");
-                    nob_cmd_append(&cmd, "-Iexternal/glfw/deps/ming");
-                    nob_cmd_append(&cmd, "-DGRAPHICS_API_OPENGL_33");
-                    if (strcmp(raylib_modules[i], "rglfw") == 0) {
-                        nob_cmd_append(&cmd, "-x", "objective-c");
-                    }
-                    nob_cmd_append(&cmd, "-c", input_path);
-                    nob_cmd_append(&cmd, "-o", output_path);
-                    break;
-                case TARGET_WIN64_MINGW:
-                    nob_cmd_append(&cmd, "x86_64-w64-mingw32-gcc");
-                    nob_cmd_append(&cmd, "-ggdb", "-DPLATFORM_DESKTOP", "-fPIC");
-                    nob_cmd_append(&cmd, "-I./raylib/raylib-"RAYLIB_VERSION"/src/external/glfw/include");
-                    nob_cmd_append(&cmd, "-c", input_path);
-                    nob_cmd_append(&cmd, "-o", output_path);
-                    break;
-                case TARGET_WIN64_MSVC:
-                    nob_cmd_append(&cmd, "cl.exe", "/DPLATFORM_DESKTOP");
-                    nob_cmd_append(&cmd, "/I", "./raylib/raylib-"RAYLIB_VERSION"/src/external/glfw/include");
-                    nob_cmd_append(&cmd, "/c", input_path);
-                    nob_cmd_append(&cmd, nob_temp_sprintf("/Fo%s", output_path));
-                    break;
-                default: NOB_ASSERT(0 && "unreachable");
-            }
-
-            Nob_Proc proc = nob_cmd_run_async(cmd);
-            nob_da_append(&procs, proc);
-        }
-    }
-    cmd.count = 0;
-    
-    if (!nob_procs_wait(procs)) nob_return_defer(false);
-    
-    switch (config.target) {
-        case TARGET_MACOS:
-        case TARGET_LINUX:
-        case TARGET_WIN64_MINGW: {
-            if (!config.hotreload) {
-                const char *libraylib_path = nob_temp_sprintf("%s/libraylib.a", build_path);
-
-                if (nob_needs_rebuild(libraylib_path, object_files.items, object_files.count)) {
-                    nob_cmd_append(&cmd, "ar", "-crs", libraylib_path);
-
-                    for (size_t i = 0; i < NOB_ARRAY_LEN(raylib_modules); ++i) {
-                        const char *input_path = nob_temp_sprintf("%s/%s.o", build_path, raylib_modules[i]);
-                        nob_cmd_append(&cmd, input_path);
-                    }
-                    if (!nob_cmd_run_sync(cmd)) nob_return_defer(false);
-                }
-            } else {
-                const char *libraylib_path = nob_temp_sprintf("%s/libraylib.so", build_path);
-
-                if (nob_needs_rebuild(libraylib_path, object_files.items, object_files.count)) {
-                    if (config.target != TARGET_LINUX) {
-                        nob_log(NOB_ERROR, "TODO: dynamic raylib for %s is not supported yet", NOB_ARRAY_GET(target_names, config.target));
-                        nob_return_defer(false);
-                    }
-                    nob_cmd_append(&cmd, "cc");
-                    nob_cmd_append(&cmd, "-shared");
-                    nob_cmd_append(&cmd, "-o", libraylib_path);
-                    for (size_t i = 0; i < NOB_ARRAY_LEN(raylib_modules); ++i) {
-                        const char *input_path = nob_temp_sprintf("%s/%s.o", build_path, raylib_modules[i]);
-                        nob_cmd_append(&cmd, input_path);
-                    }
-                    if (!nob_cmd_run_sync(cmd)) nob_return_defer(false);
-                }
-            }
-        } break;
-
-        case TARGET_WIN64_MSVC: {
-            if (!config.hotreload) {
-                const char *libraylib_path = nob_temp_sprintf("%s/raylib.lib", build_path);
-                if (nob_needs_rebuild(libraylib_path, object_files.items, object_files.count)) {
-                    nob_cmd_append(&cmd, "lib");
-                    for (size_t i = 0; i < NOB_ARRAY_LEN(raylib_modules); ++i) {
-                        const char *input_path = nob_temp_sprintf("%s/%s.obj", build_path, raylib_modules[i]);
-                        nob_cmd_append(&cmd, input_path);
-                    }
-                    nob_cmd_append(&cmd, nob_temp_sprintf("/OUT:%s", libraylib_path));
-                    if (!nob_cmd_run_sync(cmd)) nob_return_defer(false);
-                }
-            } else {
-                nob_log(NOB_WARNING, "TODO: dynamic raylib for %s is not supported yet", NOB_ARRAY_GET(target_names, config.target));
-                nob_return_defer(false);
-            }
-        } break;
-
-        default: NOB_ASSERT(0 && "unreachable");
-    }
-
-defer:
-    nob_cmd_free(cmd);
-    nob_da_free(object_files);
-    return result;
-}
-
-bool build_dist(Config config) {
-    if (config.hotreload) {
-        nob_log(NOB_ERROR, "We do not ship with hotreload enabled");
-        return false;
-    }
-
-    switch (config.target) {
-        case TARGET_LINUX: {
-            if (!nob_mkdir_if_not_exists("./musializer-linux-x86_64/")) return false;
-            if (!nob_copy_file("./build/musializer", "./musializer-linux-x86_64/musializer")) return false;
-            if (!nob_copy_directory_recursively("./resources/", "./musializer-linux-x86_64/resources/")) return false;
-            // TODO: should we pack ffmpeg with Linux build?
-            // There are some static executables for Linux
-            Nob_Cmd cmd = {0};
-            nob_cmd_append(&cmd, "tar", "fvc", "./musializer-linux-x86_64.tar.gz", "./musializer-linux-x86_64");
-            bool ok = nob_cmd_run_sync(cmd);
-            nob_cmd_free(cmd);
-            if (!ok) return false;
-        } break;
-
-        case TARGET_WIN64_MINGW: {
-            if (!nob_mkdir_if_not_exists("./musializer-win64-mingw/")) return false;
-            if (!nob_copy_file("./build/musializer.exe", "./musializer-win64-mingw/musializer.exe")) return false;
-            if (!nob_copy_directory_recursively("./resources/", "./musializer-win64-mingw/resources/")) return false;
-            if (!nob_copy_file("musializer-logged.bat", "./musializer-win64-mingw/musializer-logged.bat")) return false;
-            // TODO: pack ffmpeg.exe with windows build
-            //if (!nob_copy_file("ffmpeg.exe", "./musializer-win64-mingw/ffmpeg.exe")) return false;
-            Nob_Cmd cmd = {0};
-            nob_cmd_append(&cmd, "zip", "-r", "./musializer-win64-mingw.zip", "./musializer-win64-mingw/");
-            bool ok = nob_cmd_run_sync(cmd);
-            nob_cmd_free(cmd);
-            if (!ok) return false;
-        } break;
-
-        case TARGET_WIN64_MSVC: {
-            nob_log(NOB_ERROR, "TODO: Creating distro for MSVC build is not implemented yet");
-            return false;
-        } break;
-
-        case TARGET_MACOS: {
-            nob_log(NOB_ERROR, "TODO: Creating distro for MacOS build is not implemented yet");
-            return false;
-        }
-
-        default: NOB_ASSERT(0 && "Unreachable");
-    }
-    return true;
-}
-
-#endif // 0
-
 void log_available_subcommands(const char *program, Nob_Log_Level level) {
     nob_log(level, "Usage: %s [subcommand]", program);
     nob_log(level, "Subcommands:");
     nob_log(level, "    build (default)");
-    nob_log(level, "    config");
     nob_log(level, "    dist");
     nob_log(level, "    svg");
     nob_log(level, "    help");
 }
 
 int main(int argc, char **argv) {
-    nob_log(NOB_INFO, "-- STAGE 2 --");
+    nob_log(NOB_INFO, "--- STAGE 2 ---");
+    nob_log(NOB_INFO, "Target: %s", MUSIALIZER_TARGET_NAME);
+#ifdef MUSIALIZER_HOTRELOAD
+    nob_log(NOB_INFO, "Hotreload: ENABLED");
+#else
+    nob_log(NOB_INFO, "Hotreload: DISABLED");
+#endif // MUSIALIZER_HOTRELOAD
+
+#ifdef MUSIALIZER_MICROPHONE
+    nob_log(NOB_INFO, "Microphone: ENABLED");
+#else
+    nob_log(NOB_INFO, "Microphone: DISABLED");
+#endif //MUSIALIZER_MICROPHONE
+    nob_log(NOB_INFO, "---");
 
     const char *program = nob_shift_args(&argc, &argv);
 
@@ -331,44 +149,54 @@ int main(int argc, char **argv) {
 }
 
 #else
+void generate_default_configurattion(Nob_String_Builder *content) {
+#ifdef _WIN32
+#   if defined(_MSC_VER)
+    nob_sb_append_cstr(content, "#define MUSIALIZER_TARGET TARGET_WIN64_MSVC\n");
+#   else
+    nob_sb_append_cstr(content, "#define MUSIALIZER_TARGET TARGET_WIN64_MINGW\n");
+#   endif
+#else
+#   if defined (__APPLE__) || defined (__MACH__)
+    nob_sb_append_cstr(content, "#define MUSIALIZER_TARGET TARGET_MACOS\n");
+#   else
+    nob_sb_append_cstr(content, "#define MUSIALIZER_TARGET TARGET_LINUX\n");
+#   endif
+#endif
+    nob_sb_append_cstr(content, "// #define MUSIALIZER_HOTRELOAD\n");
+    nob_sb_append_cstr(content, "// #define MUSIALIZER_MICROPHONE\n");
+}
+
 int main(int argc, char **argv) {
     NOB_GO_REBUILD_URSELF(argc, argv);
 
-    nob_log(NOB_INFO, "-- STAGE 1 --");
+    nob_log(NOB_INFO, "--- STAGE 1 ---");
 
     const char *program = nob_shift_args(&argc, &argv);
     if (!nob_mkdir_if_not_exists("build")) return 1;
 
-    const char *config_path = "./build/config.h";
-    int config_exists = nob_file_exists(config_path);
+    int config_exists = nob_file_exists(CONFIG_PATH);
     if (config_exists < 0) return 1;
     if (config_exists == 0) {
-        nob_log(NOB_INFO, "Generating %s", config_path);
+        nob_log(NOB_INFO, "Generating %s", CONFIG_PATH);
         Nob_String_Builder content = {0};
-#ifdef _WIN32
-#   if defined(_MSC_VER)
-        nob_sb_append_cstr(&content, "#define MUSIALIZER_TARGET TARGET_WIN64_MSVC\n");
-#   else
-        nob_sb_append_cstr(&content, "#define MUSIALIZER_TARGET TARGET_WIN64_MINGW\n");
-#   endif
-#else
-#   if defined (__APPLE__) || defined (__MACH__)
-        nob_sb_append_cstr(&content, "#define MUSIALIZER_TARGET TARGET_MACOS\n");
-#   else
-        nob_sb_append_cstr(&content, "#define MUSIALIZER_TARGET TARGET_LINUX\n");
-#   endif
-#endif
-        nob_sb_append_cstr(&content, "// #define MUSIALIZER_HOTRELOAD\n");
-        nob_sb_append_cstr(&content, "// #define MUSIALIZER_MICROPHONE\n");
-        if (!nob_write_entire_file(config_path, content.items, content.count)) return 1;
+        generate_default_configurattion(&content);
+        if (!nob_write_entire_file(CONFIG_PATH, content.items, content.count)) return 1;
     } else {
-        nob_log(NOB_INFO, "%s already exists", config_path);
+        nob_log(NOB_INFO, "File `%s` already exists", CONFIG_PATH);
     }
 
     Nob_Cmd cmd = {0};
     const char *configured_binary = "./build/nob.configured";
-    nob_cmd_append(&cmd, NOB_REBUILD_URSELF(configured_binary, "nob.c"), "-DCONFIGURED");
-    if (!nob_cmd_run_sync(cmd)) return 1;
+    const char *deps[] = { __FILE__, CONFIG_PATH };
+    int needs_rebuild = nob_needs_rebuild(configured_binary, deps, NOB_ARRAY_LEN(deps));
+    if (needs_rebuild < 0) return 1;
+    if (needs_rebuild) {
+        nob_cmd_append(&cmd, NOB_REBUILD_URSELF(configured_binary, "nob.c"), "-DCONFIGURED");
+        if (!nob_cmd_run_sync(cmd)) return 1;
+    } else {
+        nob_log(NOB_INFO, "executable `%s` is up to date", configured_binary);
+    }
 
     cmd.count = 0;
     nob_cmd_append(&cmd, configured_binary);

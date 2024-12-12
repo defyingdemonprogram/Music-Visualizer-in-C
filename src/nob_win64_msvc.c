f46db8dc3e5a12f4b9fd58849599cf839550d296
sanjeev.bhandari <sanjeev.bhandari@tai.com.np>
2024-12-12 22:31:10 +0545

    fix the issue for building in other platform other than linux

--- src/nob_win64_msvc.c
+++ src/nob_win64_msvc.c
@@ -6,7 +6,7 @@ bool build_musializer(void) {
     Nob_Procs procs = {0};
 
 #ifdef MUSIALIZER_HOTRELOAD
-    nob_log(NOB_ERROR, "TODO: hotreloading is not supported on %s yet", NOB_ARRAY_GET(target_names, config.target));
+    nob_log(NOB_ERROR, "TODO: hotreloading is not supported on %s yet", NOB_ARRAY_GET(MUSIALIZER_TARGET_NAME));
     nob_return_defer(false);
 #else
     cmd.count = 0;
@@ -52,7 +52,7 @@ bool build_raylib(void) {
 
     Nob_Procs procs = {0};
 
-    const char *build_path = nob_temp_sprintf("./build/raylib/%s", NOB_ARRAY_GET(target_names, config.target));
+    const char *build_path = nob_temp_sprintf("./build/raylib/%s", NOB_ARRAY_GET(MUSIALIZER_TARGET_NAME));
 
     if (!nob_mkdir_if_not_exists(build_path)) {
         nob_return_defer(false);
@@ -90,7 +90,7 @@ bool build_raylib(void) {
         if (!nob_cmd_run_sync(cmd)) nob_return_defer(false);
     }
 #else
-    nob_log(NOB_WARNING, "TODO: dynamic raylib for %s is not supported yet", NOB_ARRAY_GET(target_names, config.target));
+    nob_log(NOB_WARNING, "TODO: dynamic raylib for %s is not supported yet", NOB_ARRAY_GET(MUSIALIZER_TARGET_NAME));
     nob_return_defer(false);
 #endif // MUSIALIZER_HOTRELOAD
 
